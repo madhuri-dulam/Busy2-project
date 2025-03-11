4e7cad74cce688ff0140d6719220e1c648af2bba
madhuri-dulam <madhuridulam23@gmail.com>
2025-03-11 18:53:55 +0530

    Updated project in development branch

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from "react-redux";
+
 import { store } from "./store";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
